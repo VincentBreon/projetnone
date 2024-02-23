70f2ed5eb0c00c1f5b7e3a234f7c751800803604
Vincent Breon <vincentbreon@MacBook-Pro-de-Vincent.local>
2024-02-23 11:25:21 +0100

    ajout de navbar bootstrap

--- projet-node/server.js
+++ projet-node/server.js
@@ -1,11 +1,18 @@
-let express = require('express');
+let express = require("express");
 let app = express();
 let port = 3000;
+
+app.use(express.static(__dirname + "/www"));
+app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
+app.use("/js", express.static(__dirname + "/node_modules/bootstrap/dist/js"));
+app.use("/js", express.static(__dirname + "/node_modules/jquery/dist"));
+
+
 
 app.listen(port, () => {
   console.log("Le serveur est en route");
   console.log(`Serveur listening at http://localhost:${port}`);
 });
 app.get("/", (req, res, next) => {
-  res.send("Bonjour !");
+  res.sendFile("/www/index.html");
 });
